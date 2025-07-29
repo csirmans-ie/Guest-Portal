4104e78800de4fb34ec5f23e74091db2240b6133
csirmans-ie <csirmans@beemok.com>
2025-07-29 15:39:22 -0400

    Update meraki.js

--- meraki.js
+++ meraki.js
@@ -1,55 +1,4 @@
-// Parse paramaters
-var base_grant_url = decodeURIComponent(GetURLParameter("base_grant_url"));
-var user_continue_url = decodeURIComponent(GetURLParameter("user_continue_url"));
-var node_mac = GetURLParameter("node_mac");
-var client_ip = GetURLParameter("client_ip");
-var client_mac = GetURLParameter("client_mac");
-
-// Print Meraki provided paramaters for Debugging State
-console.log("user_continue_url: "+user_continue_url);
-console.log("client_ip: "+client_ip);
-document.getElementById("baseGrantURL").innerHTML = base_grant_url;
-document.getElementById("userContinueURL").innerHTML = user_continue_url;
-document.getElementById("clientIP").innerHTML = client_ip;
-document.getElementById("clientMAC").innerHTML = client_mac;
-document.getElementById("nodeMAC").innerHTML = node_mac;
-
-// Form Submit handler. 
-document.getElementById('loginForm').onsubmit= function(e){
-    e.preventDefault(); //prevents default form submission process to allow login and validation
-    login();
-}
-
-// ******************
-// Login to Meraki by redirecting client to the base_grant_url 
-// 
-// The logingUrl will add a continue_url parameter for a final client
-// redirect to their intended site. 
-// (you could override this url to send the user to a home page)
-// ****************** 
-function authUser(){
-
-    var loginUrl = base_grant_url;
-    if(user_continue_url !== "undefined"){
-        loginUrl += "?continue_url="+user_continue_url;
-    }
-    console.log("Logging in... ",loginUrl);
-    // redirect browser to meraki auth URL.
-    window.location.href = loginUrl;
-}
-
-// Button handler function to store the form data and login. 
-function login(){
-    // send the data somewhere like a database
-    var data = {};
-    data.name = document.getElementById("name").value;
-    data.email = document.getElementById("email").value;
-    alert("Hello "+data.name +"\n"+"Thanks for providing your email: "+data.email);
-    console.log("Storing data to db...", data);
-
-    // Complete Login
-    authUser();
-}
+// Path: meraki.js
 
 // Helper function to parse URL
 function GetURLParameter(sParam)
@@ -65,3 +14,43 @@ function GetURLParameter(sParam)
         }
     }
 }
+
+// Parse URL parameters for Meraki appended items
+var base_grant_url = decodeURIComponent(GetURLParameter("base_grant_url"));
+var user_continue_url = decodeURIComponent(GetURLParameter("user_continue_url"));
+var node_mac = GetURLParameter("node_mac");
+var client_ip = GetURLParameter("client_ip");
+var client_mac = GetURLParameter("client_mac");
+
+// Output Meraki API parameters to console for debugging
+console.log("base_grant_url: " + base_grant_url);
+console.log("user_continue_url: " + user_continue_url);
+console.log("node_mac: " + node_mac);
+console.log("client_ip: " + client_ip);
+console.log("client_mac: " + client_mac);
+
+// Function to redirect user to Meraki auth URL
+function authUser(){
+  var loginUrl = base_grant_url;
+
+  // Check if user continue URL is defined
+  if(user_continue_url !== "undefined")
+      loginUrl += "?continue_url=" + user_continue_url;
+  
+  // Redirect user to Meraki auth URL
+  window.location.href = loginUrl;
+}
+
+// Function to store user entered data and start auth
+function login(){
+
+  // **************************
+  // store user data to db here
+  // **************************
+
+  // Start user auth to Meraki
+  authUser();
+}
+
+// Add click listener to continue button to login user
+document.getElementById('continue-btn').onclick = function() {login()}
